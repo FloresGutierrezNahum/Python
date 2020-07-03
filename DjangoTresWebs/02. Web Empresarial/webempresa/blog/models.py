b6ab60e45b6b1e6fce2a4b6d7b33d3aab81a42cd
NahumFG <36179329+FloresGutierrezNahum@users.noreply.github.com>
2020-07-03 18:15:55 -0500

    enlaces por llaves foraneas compeltado

--- DjangoTresWebs/02. Web Empresarial/webempresa/blog/models.py
+++ DjangoTresWebs/02. Web Empresarial/webempresa/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.utils import timezone
+from django.utils.timezone import now
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -19,10 +20,10 @@ class Category(models.Model):
 class Post(models.Model):
     title      = models.CharField(max_length=200, verbose_name="Titulo")
     content    = models.TextField(verbose_name="contenido")
-    published  = models.DateTimeField(verbose_name="Fecha de publicación", default = timezone.now())
+    published  = models.DateTimeField(verbose_name="Fecha de publicación", default = now)
     image      = models.ImageField(verbose_name="Imagen", upload_to="blog", null=True, blank=True)
-    author     = 
-    categories = 
+    author     = models.ForeignKey(User, verbose_name="Autor", on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category, verbose_name = "Categorias")
     created    = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated    = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")   
 
