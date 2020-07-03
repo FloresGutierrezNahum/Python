2194a223591314ead7c920bc55fe6b44dc717486
NahumFG <36179329+FloresGutierrezNahum@users.noreply.github.com>
2020-07-03 11:44:42 -0500

    Muestra de imagenes en servidor de pruebas

--- DjangoTresWebs/01. Proyecto Básico/webpersonal/webpersonal/urls.py
+++ DjangoTresWebs/01. Proyecto Básico/webpersonal/webpersonal/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 
+from django.conf import settings #Carga la configuración incluyendo MEDIA_URL y MEDIA_ROOT
+
 urlpatterns = [
     path('',views.home, name='home'),
     path('about-me/',views.about, name='about'),
@@ -24,3 +26,8 @@ urlpatterns = [
     path('contact/',views.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
+
+#Configuración extendida para abrir imágenes
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
