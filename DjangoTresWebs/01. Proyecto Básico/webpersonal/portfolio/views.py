3a2b6c9ab9e8fe90440c0ce1b2e52f6230e9580d
NahumFG <36179329+FloresGutierrezNahum@users.noreply.github.com>
2020-07-03 12:11:32 -0500

    Error de sintaxis solucionado

--- DjangoTresWebs/01. Proyecto Básico/webpersonal/portfolio/views.py
+++ DjangoTresWebs/01. Proyecto Básico/webpersonal/portfolio/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def portfolio(request):
-    return render(request, "portfolio/portfolio.html")
+    projects = Project.objects.all() #Devuelve todos los objetos que tiene el modelo de project
+    #return render(request, "portfolio/portfolio.html")
+    return render(request, "portfolio/portfolio.html", {'projects':projects})
