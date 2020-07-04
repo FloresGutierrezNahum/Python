e95ab6d9a090facb7f2e92eb09725770a4160087
NahumFG <36179329+FloresGutierrezNahum@users.noreply.github.com>
2020-07-04 17:55:48 -0500

    apps terminadas

--- DjangoTresWebs/02. Web Empresarial/webempresa/webempresa/urls.py
+++ DjangoTresWebs/02. Web Empresarial/webempresa/webempresa/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-#Carga la configuración incluyendo MEDIA_URL y MEDIA_ROOT
-from django.conf import settings 
+from django.conf import settings
 
 urlpatterns = [
     # Paths del core
@@ -28,11 +26,12 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     # Paths de pages
     path('page/', include('pages.urls')),
+    # Paths de pages
+    path('contact/', include('contact.urls')),
     # Paths del admin
     path('admin/', admin.site.urls),
 ]
 
-#Configuración extendida para abrir imágenes
 if settings.DEBUG:
     from django.conf.urls.static import static
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT )
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
